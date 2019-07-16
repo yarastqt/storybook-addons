944cf478d07d5df8bd2e84d33c6d1866fe19a2cd
yarastqt <yarastqt@gmail.com>
2019-07-17 00:19:57 +0300

    feat(docs): add markdown placeholders

--- packages/docs/src/index.ts
+++ packages/docs/src/index.ts
@@ -3,18 +3,35 @@ import addons, { StoryGetter, StoryContext, makeDecorator } from '@storybook/add
 import { ADD_README } from './constants'
 
 export type WithDocsOptions = {
-  readme?: string
+  readme?: {
+    content: string
+    placeholders?: Record<string, string>
+  }
 }
 
 const registerCacheMap = new Map<string, boolean>()
 
-export const withDocs = (options: WithDocsOptions) => makeDecorator({
+const injectMarkdownPlaceholders = (content: string, placeholders: Record<string, string> = {}) => {
+  return content.replace(/{{%inject:(.+.)%}}/g, (match: string, key: string) => {
+    if (placeholders[key] !== undefined) {
+      return placeholders[key]
+    }
+    return ''
+  })
+}
+
+export const withDocs = ({ readme }: WithDocsOptions) => makeDecorator({
   name: 'withDocs',
   parameterName: 'docs',
   wrapper: (getStory: StoryGetter, context: StoryContext) => {
     if (!registerCacheMap.has(context.kind)) {
-      addons.getChannel().emit(ADD_README, options.readme)
-      registerCacheMap.set(context.kind, true)
+      if (readme !== undefined) {
+        addons.getChannel().emit(
+          ADD_README,
+          injectMarkdownPlaceholders(readme.content, readme.placeholders),
+        )
+        registerCacheMap.set(context.kind, true)
+      }
     }
     return getStory(context)
   },
