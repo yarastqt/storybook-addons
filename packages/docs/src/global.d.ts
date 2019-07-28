d9c82988b2f6e9c60dcb189c1074bb5182b832e3
yarastqt <yarastqt@gmail.com>
2019-07-28 15:08:40 +0300

    chore(docs): remove unused global typings
    Add typings for react-markdown for html mode

--- packages/docs/src/global.d.ts
+++ packages/docs/src/global.d.ts
@@ -1,15 +1,4 @@
-declare module 'remark-parse' {
-  export default any
-}
-
-declare module 'remark-react' {
-  export default any
-}
-
-declare module 'remark-slug' {
-  export default any
-}
-
-declare module 'remark-autolink-headings' {
-  export default any
+declare module 'react-markdown/with-html' {
+  import ReactMarkdown from 'react-markdown'
+  export default ReactMarkdown
 }
