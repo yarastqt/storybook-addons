80334ec03356cbf9a8a63fb897230a7c47ad4347
yarastqt <yarastqt@gmail.com>
2020-02-29 23:22:23 +0300

    fix(docs): set visible anchor in focus

--- packages/docs/src/components/typo.tsx
+++ packages/docs/src/components/typo.tsx
@@ -50,20 +50,22 @@ export const typo = css`
     opacity: 0;
     transition: opacity 100ms ease-in-out;
 
+    &::before {
+      content: '';
+      background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M7.075 8.449a1 1 0 0 1-1.63 1.158 4 4 0 0 1 .434-5.142L8.886 1.46a4 4 0 0 1 5.657 5.657l-.707.707A1 1 0 0 1 12.42 6.41l.707-.707A2 2 0 1 0 10.3 2.874L7.294 5.88a2 2 0 0 0-.22 2.57zm1.848-.901a1 1 0 1 1 1.627-1.164 4 4 0 0 1-.428 5.152l-3.005 3.005A4 4 0 1 1 1.46 8.886l.707-.707a1 1 0 1 1 1.414 1.414l-.707.707a2 2 0 1 0 2.829 2.828l3.005-3.005a2 2 0 0 0 .215-2.574z' fill='%23000000'%3E%3C/path%3E%3C/svg%3E");
+      display: inline-block;
+      height: 16px;
+      width: 16px;
+      opacity: 0.5;
+      transition: opacity 100ms ease-in-out;
+      vertical-align: middle;
+    }
+
+    &:focus,
+    &:focus::before,
     &:hover::before {
       opacity: 1;
     }
-  }
-
-  .anchor::before {
-    content: '';
-    background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M7.075 8.449a1 1 0 0 1-1.63 1.158 4 4 0 0 1 .434-5.142L8.886 1.46a4 4 0 0 1 5.657 5.657l-.707.707A1 1 0 0 1 12.42 6.41l.707-.707A2 2 0 1 0 10.3 2.874L7.294 5.88a2 2 0 0 0-.22 2.57zm1.848-.901a1 1 0 1 1 1.627-1.164 4 4 0 0 1-.428 5.152l-3.005 3.005A4 4 0 1 1 1.46 8.886l.707-.707a1 1 0 1 1 1.414 1.414l-.707.707a2 2 0 1 0 2.829 2.828l3.005-3.005a2 2 0 0 0 .215-2.574z' fill='%23000000'%3E%3C/path%3E%3C/svg%3E");
-    display: inline-block;
-    height: 16px;
-    width: 16px;
-    opacity: 0.5;
-    transition: opacity 100ms ease-in-out;
-    vertical-align: middle;
   }
 
   p,
