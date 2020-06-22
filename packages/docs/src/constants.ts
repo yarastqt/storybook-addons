968dc00e132033898044946c4b7887df6724632c
yarastqt <yarastqt@gmail.com>
2020-06-22 19:09:30 +0300

    feat: remove legacy API
    BREAKING CHANGE: remove legacy API

--- packages/docs/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const ADDON_ID = '@storybook-addons/docs'
-export const PARAM_KEY = '@storybook-addons/docs'
-export const PANEL_ID = `${ADDON_ID}/panel`
-export const ADD_README = '@storybook-addons/add/readme'
